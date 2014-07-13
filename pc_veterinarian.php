8fc9e4ffbf6a37b59d7e9ff900c1f1a4f52ff010
Axwell90 aaa <Axwell90@yandex.ru>
2014-07-13 21:16:58 +0400

    doc table

---
+++ pc_veterinarian.php
@@ -0,0 +1,30 @@
+<?php 
+
+//src/Doc/StoreBundle/Entity/pc_veterinarian.php
+namespace Doc\StoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+*ORM\Entity
+*ORM\Table(name="veterinarian")
+*/
+class veterinarian
+{
+	/**
+	*ORM\Id
+	*ORM\Column(type="integer", unique=true)
+	*ORM\GeneratedValue(strategy="AUTO")
+	*/
+	protected $id;
+
+	/**
+	*ORM\Column(type="string", length=16)
+	*/
+	protected $name;
+
+	/**
+	*ORM\Column(type="string", length=16)
+	*/
+	protected $profession;
+}
